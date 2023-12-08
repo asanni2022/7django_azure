304bb61bad2ed7d3de78c236eaa8be86ba3f671c
asanni2022 <akeem.sql01@gmail.com>
2023-12-08 07:27:09 -0500

    modified message

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def home_page_view(request):
-    return HttpResponse("Hello, World! from docker container django")
+    return HttpResponse("Hello, World! from docker container django with CICD deployment")
 
 
 
